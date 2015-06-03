36983aef830415da1abaa123940e995bb7f58109
Ansel Rosenberg <ansel01@gmail.com>
2015-06-02 17:14:07 -0700

    Switch to express app

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 var http = require("http");
+var express = require('express');
+var app = express();
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(process.env.PORT || 8888);
+// http.createServer(function(request, response) {
+//   response.writeHead(200, {"Content-Type": "text/plain"});
+//   response.write("Hello World");
+//   response.end();
+// }).listen(process.env.PORT || 8888);
+
+app.get('/', function(req, res) {
+  res.statusCode(200);
+  res.write('Express app');
+  res.end();
+
+})
+
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+  console.log('Listening on port', port)
+});
